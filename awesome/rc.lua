6b560b3bb7ec0071ba576240e95573d39964cfe9
Erika Suárez Valencia <marvinesv@gmail.com>
2013-07-01 19:21:43 -0500

    comentado el dropbox start

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -496,4 +496,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.util.spawn_with_shell("dropbox start")
+-- awful.util.spawn_with_shell("dropbox start")
